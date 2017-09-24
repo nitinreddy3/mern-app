29dec3edfc43c4f1ff59db629ecca8475409d166
Nitin Reddy <reddynitin3@gmail.com>
2017-09-24 11:59:56 +0530

    Setup complete to add items:

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+import AddItem from './components/AddItem';
+import IndexItem from './components/IndexItem';
+import EditItem from './components/EditItem';
+
+ReactDOM.render(
+  <Router>
+      <div>
+        <Route exact path='/' component={App} />
+        <Route path='/add-item' component={AddItem} />
+        <Route path='/index' component={IndexItem} />
+        <Route path='/edit/:id' component={EditItem} />
+      </div>
+  </Router>,
+  document.getElementById('root')
+);
